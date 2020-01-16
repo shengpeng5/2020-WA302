9589e00793787b64023449305511e200f6c0f01f
shengpeng5 <59590839+shengpeng5@users.noreply.github.com>
2020-01-15 17:32:28 -0700

    1/15 01

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal Mile;
+            decimal Kilometre;
+            decimal TotalM = 0.0m;
+            decimal TotalKM = 0.0m;
+
+            const decimal ImperialTotalKMValue = 1.60934m;
+
+            Mile = 3m;
+
+            TotalKM = Mile * ImperialTotalKMValue;
+            Console.WriteLine("According to your Mile {0:0.0}: your Total KM is {1:0.00} ", Mile, TotalKM);
+
+
+            Kilometre = 3m;
+
+            const decimal ImperialTotalMValue = 0.621371m;
+
+            TotalM = Kilometre * ImperialTotalMValue;
+            Console.WriteLine("According to your Kilometre {0:0.0}: your Total M is {1:0.00}", Kilometre, TotalM);
+
+        }
+    }
+}
